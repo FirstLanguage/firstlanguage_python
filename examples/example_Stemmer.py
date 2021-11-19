08229f3c26bd53e38125aa7901ac2ff53776cbf0
Subalalitha <subalalitha@firstlanguage.in>
2021-11-19 13:01:04 +0530

    Packaged

---
+++ examples/example_Stemmer.py
@@ -0,0 +1,19 @@
+from firstlanguage.firstlanguage_client import Client
+from firstlanguage.configuration import Environment
+import jsonpickle
+
+
+client = Client(
+    apikey='079cc45f-bbb2-4f01-ac27-63ff9a2d82b5',
+    environment=Environment.PRODUCTION,)
+
+reqbody='{"input":{"text":"அவள் வேகமாக ஓடினாள்","lang":"ta"} }'
+
+body = jsonpickle.decode(reqbody)
+basic_api_controller = client.basic_api
+
+result = basic_api_controller.get_stemmer(body)
+
+for res in result:
+  print("Original Text passed: "+res.orginal_text)
+  print("Stemmed result: "+res.stem)
